769ce06f57d2b1a6c45a1eb06e1f9b08ce0bd4a5
Kenneth Bolima <kenneth@mykindred.co>
2025-01-20 14:17:35 +0800

    set default host and port to 0.0.0.0 and 8080

--- auth/src/config/config.rs
+++ auth/src/config/config.rs
@@ -2,15 +2,16 @@ use std::env;
 use dotenv::dotenv;
 
 pub fn get_host() -> String {
-    env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string())
+    dotenv().ok();
+    env::var("HOST").unwrap_or_else(|_| "0.0.0.0".to_string())
 }
 
 pub fn get_port() -> String {
+    dotenv().ok();
     env::var("PORT").unwrap_or_else(|_| "8080".to_string())
 }
 
 pub fn get_laravel_url() -> String {
     dotenv().ok();
-
     env::var("LARAVEL_URL").expect("Laravel URL must be set!")
 }
